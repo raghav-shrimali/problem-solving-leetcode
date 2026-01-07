88c7a2769f7f441eabe4365810803210302ff197
Raghav Shrimali <141941073+raghav-shrimali@users.noreply.github.com>
2026-01-07 14:43:12 +0530

    Add gas station problem solution
    Implement solution for LeetCode 134: Gas Station using a greedy approach.

---
+++ Arrays/gas_station.cpp
@@ -0,0 +1,21 @@
+// LeetCode 134: Gas Station
+// Approach: Greedy
+// Time: O(n), Space: O(1)
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int total = 0, curr = 0, start = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            total += gas[i] - cost[i];
+            curr += gas[i] - cost[i];
+
+            if (curr < 0) {
+                start = i + 1;
+                curr = 0;
+            }
+        }
+        return total >= 0 ? start : -1;
+    }
+};
