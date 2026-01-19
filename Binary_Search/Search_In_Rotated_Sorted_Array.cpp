22c96fd27a00e32e5f2b9ccf22c9e9232a1c0aca
Raghav Shrimali <141941073+raghav-shrimali@users.noreply.github.com>
2026-01-19 17:59:20 +0530

    Create Search_In_Rotated_Sorted_Array.cpp

---
+++ Binary_Search/Search_In_Rotated_Sorted_Array.cpp
@@ -0,0 +1,40 @@
+/*
+LeetCode: 33 - Search in Rotated Sorted Array
+
+Approach:
+- Modified binary search
+- Identify sorted half and search accordingly
+
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            } else {
+                if (target > nums[mid] && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
