20acc30d734323042e66f9593545a2d3bc7722ca
Raghav Shrimali <141941073+raghav-shrimali@users.noreply.github.com>
2026-02-07 12:41:30 +0530

    Create Minimum_Depth_of_Binary_Tree.cpp

---
+++ Binary-Tree-BST/Minimum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,19 @@
+/*
+LeetCode 111 - Minimum Depth of Binary Tree
+
+Approach: DFS
+Time Complexity: O(n)
+Space Complexity: O(h)
+*/
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root) return 0;
+
+        if (!root->left) return 1 + minDepth(root->right);
+        if (!root->right) return 1 + minDepth(root->left);
+
+        return 1 + min(minDepth(root->left), minDepth(root->right));
+    }
+};
