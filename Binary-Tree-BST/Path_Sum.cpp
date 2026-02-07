a34e1704cccd5fb186007dfafd7a4ff1b6accaab
Raghav Shrimali <141941073+raghav-shrimali@users.noreply.github.com>
2026-02-07 12:42:05 +0530

    Create Path_Sum.cpp

---
+++ Binary-Tree-BST/Path_Sum.cpp
@@ -0,0 +1,20 @@
+/*
+LeetCode 112 - Path Sum
+
+Approach: DFS
+Time Complexity: O(n)
+Space Complexity: O(h)
+*/
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root) return false;
+
+        if (!root->left && !root->right)
+            return targetSum == root->val;
+
+        return hasPathSum(root->left, targetSum - root->val) ||
+               hasPathSum(root->right, targetSum - root->val);
+    }
+};
