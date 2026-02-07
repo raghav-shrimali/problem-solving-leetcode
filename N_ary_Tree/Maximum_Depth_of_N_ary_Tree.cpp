587db6bd9b0f8a0fe9f3081c811ee0052a350978
Raghav Shrimali <141941073+raghav-shrimali@users.noreply.github.com>
2026-02-07 12:45:44 +0530

    Create Maximum_Depth_of_N_ary_Tree.cpp

---
+++ N_ary_Tree/Maximum_Depth_of_N_ary_Tree.cpp
@@ -0,0 +1,20 @@
+/*
+LeetCode 559 - Maximum Depth of N-ary Tree
+
+Approach: DFS
+Time Complexity: O(n)
+Space Complexity: O(h)
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (!root) return 0;
+
+        int depth = 0;
+        for (auto child : root->children)
+            depth = max(depth, maxDepth(child));
+
+        return 1 + depth;
+    }
+};
