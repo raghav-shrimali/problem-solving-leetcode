64ea31dde6b8a0d6738c217574e7c95fbe104ea0
Raghav Shrimali <141941073+raghav-shrimali@users.noreply.github.com>
2026-02-09 22:34:37 +0530

    Create Count_Binary_Substrings.cpp

---
+++ Strings/Count_Binary_Substrings.cpp
@@ -0,0 +1,24 @@
+/*
+LeetCode 696 - Count Binary Substrings
+
+Approach:
+- Count consecutive groups of 0s and 1s.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int prev = 0, curr = 1, ans = 0;
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] == s[i-1]) curr++;
+            else {
+                ans += min(prev, curr);
+                prev = curr;
+                curr = 1;
+            }
+        }
+        return ans + min(prev, curr);
+    }
+};
