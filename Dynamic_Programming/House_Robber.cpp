1c6d287bf3bd36ac5bf43d297b208d0af967c5db
Raghav Shrimali <141941073+raghav-shrimali@users.noreply.github.com>
2026-04-15 19:59:32 +0530

    Create House_Robber.cpp

---
+++ Dynamic_Programming/House_Robber.cpp
@@ -0,0 +1,31 @@
+/*
+LeetCode Problem: 198
+Title: House Robber
+
+Approach:
+- DP (include/exclude)
+
+Time Complexity: O(N)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        if(nums.size() == 1)
+            return nums[0];
+        
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
+        
+        for(int i = 2; i < nums.size(); i++)
+        {
+            int curr = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return prev1;
+    }
+};
