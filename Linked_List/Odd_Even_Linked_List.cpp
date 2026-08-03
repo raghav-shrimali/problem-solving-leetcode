7bee8fa50f7c710d39212dd9728c1d38bc43b18b
Raghav Shrimali <141941073+raghav-shrimali@users.noreply.github.com>
2026-08-03 15:52:34 +0530

    Create Odd_Even_Linked_List.cpp

---
+++ Linked_List/Odd_Even_Linked_List.cpp
@@ -0,0 +1,50 @@
+/*
+Question: 328. Odd Even Linked List
+
+Topic:
+- Linked List
+
+Approach:
+- Separate odd-indexed and even-indexed nodes.
+- Connect all odd nodes together.
+- Connect all even nodes together.
+- Append the even list after the odd list.
+
+Time Complexity: O(N)
+Space Complexity: O(1)
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+
+        while (even != nullptr && even->next != nullptr) {
+            odd->next = even->next;
+            odd = odd->next;
+
+            even->next = odd->next;
+            even = even->next;
+        }
+
+        odd->next = evenHead;
+
+        return head;
+    }
+};
